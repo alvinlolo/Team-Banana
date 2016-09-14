fb1554918643cd08929f12cb2e85cb7503640228
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-09-14 11:46:01 +1000

    basic video and comments demo

---
+++ demo1/comments.js
@@ -0,0 +1,8 @@
+comments = {
+    0: ["nice dock"],
+    1: ["looks like a nice day", "what's the temp?"],
+    5: ["would love to own a boat"],
+    6: ["oh damn!"],
+    7: ["would love to go on a cruise", "anyone know where this is?"],
+    8: ["It's somewhere in italy"],
+}
