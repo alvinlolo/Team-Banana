e08416f698123a88cd2e657e257b0b4f577b1d6b
Zac Strauss <zstra7@hotmail.com>
2016-10-18 21:36:14 +1000

    modified range style

--- demo4/comments.js
+++ demo4/comments.js
@@ -1,7 +1,7 @@
 comments = {
     0: ["nice dock this is really long comment, really long, really really long"],
     1: ["looks like a nice day", "what's the temp?"],
-    5: ["would love to own a boat"],
+    5: ["would love to own a boat","yeah man"],
     6: ["oh damn!"],
     7: ["would love to go on a cruise", "anyone know where this is?"],
     8: ["It's somewhere in italy"],
