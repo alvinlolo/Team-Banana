399cb325af45fa0ce2f0ba4166b97ecea5a25624
Zac Strauss <zstra7@hotmail.com>
2016-10-28 13:14:23 +1000

    Multiple videos work with comments and heatmap

--- demo4/comments.js
+++ demo4/comments.js
@@ -1,11 +1,22 @@
-comments = {
-    0: ["nice dock this is really long comment, really long, really really long"],
-    1: ["looks like a nice day", "what's the temp?"],
-    5: ["would love to own a boat","yeah man"],
-    6: ["oh damn!"],
-    7: ["would love to go on a cruise", "anyone know where this is?"],
-    8: ["It's somewhere in italy"],
-    15: ["That's a lot of water", "Those boats are going to get owned", "nice", "cool", "I can't believe this."],
-    30: ["Oh damn!", "wow"],
-    60: ["Oh damn!", "wow", "cool", "awesome"]
-}
+commentsArray = [
+	{
+    	0: ["nice dock this is really long comment, really long, really really long"],
+    	1: ["looks like a nice day", "what's the temp?"],
+    	5: ["would love to own a boat","yeah man"],
+    	6: ["oh damn!"],
+    	7: ["would love to go on a cruise", "anyone know where this is?"],
+    	8: ["It's somewhere in italy"],
+    	15: ["That's a lot of water", "Those boats are going to get owned", "nice", "cool", "I can't believe this."],
+    	30: ["Oh damn!", "wow"],
+    	60: ["Oh damn!", "wow", "cool", "awesome"]
+	}, 
+	{
+  
+    	30: ["Oh damn!", "wow"],
+
+	}
+];
+
+headings = ["Cruise Ship Does Damage", "Bizzare Snow"];
+
+
