7bf9cb356f2c5afaccd805d4fb58cc8554a13582
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-10-30 11:22:18 +1000

    comments have votes

--- demo4/comments.js
+++ demo4/comments.js
@@ -1,22 +1,48 @@
 commentsArray = [
 	{
-    	0: ["nice dock this is really long comment, really long, really really long"],
-    	1: ["looks like a nice day", "what's the temp?"],
-    	5: ["would love to own a boat","yeah man"],
-    	6: ["oh damn!"],
-    	7: ["would love to go on a cruise", "anyone know where this is?"],
-    	8: ["It's somewhere in italy"],
-    	15: ["That's a lot of water", "Those boats are going to get owned", "nice", "cool", "I can't believe this."],
-    	30: ["Oh damn!", "wow"],
-    	60: ["Oh damn!", "wow", "cool", "awesome"]
-	}, 
+    	0: {
+			"comment": ["nice dock this is really long comment, really long, really really long"],
+			"vote": 6
+		},
+    	1: {
+			"comment": ["looks like a nice day", "what's the temp?"],
+			"vote": 5
+		},
+    	5: {
+			"comment": ["would love to own a boat","yeah man"],
+			"vote": 1
+		},
+    	6: {
+			"comment": ["oh damn!"],
+			"vote": 50
+		},
+    	7: {
+			"comment": ["would love to go on a cruise", "anyone know where this is?"],
+			"vote": 3
+		},
+    	8: {
+			"comment": ["It's somewhere in italy"],
+			"vote": 80
+		},
+    	15: {
+			"comment": ["That's a lot of water", "Those boats are going to get owned", "nice", "cool", "I can't believe this."],
+			"vote": 1
+		},
+    	30: {
+			"comment": ["Oh damn!", "wow"],
+			"vote": 5
+		},
+    	60: {
+			"comment": ["Oh damn!", "wow", "cool", "awesome"],
+			"vote": 5
+		}
+	},
 	{
-  
-    	30: ["Oh damn!", "wow"],
-
+    	30: {
+			"comment": ["Oh damn!", "wow"],
+			"vote": 3
+		}
 	}
 ];
 
 headings = ["Cruise Ship Does Damage", "Bizzare Snow"];
-
-
